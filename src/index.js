a07fd00f2ceeca3260582c1362aa56e99dd1e457
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-06-09 18:22:40 -0400

    some fixes

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
     <ThemeContextProvider>
       <App />
-    </ThemeContextProvider>,
+    </ThemeContextProvider>
+  </Router>,
   document.getElementById('root')
 );
 
