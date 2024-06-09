c29b73ec48663271de689486eb8bf437647b0d6b
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-06-09 16:30:51 -0400

    new comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import { ThemeContext } from './contexts/ThemeContext';
-import { Main, BlogPage, ProjectPage } from './pages'
+import { Main, ProjectPage } from './pages'
 import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
 
@@ -18,8 +18,7 @@ function App() {
       <Router>
         <ScrollToTop/>
         <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/blog" exact component={BlogPage} />
+          <Route path="/Shravya" exact component={Main} />
           <Route path="/projects" exact component={ProjectPage} />
 
           <Redirect to="/" />
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Services, Achievement } from '../../components'
+import { Navbar, Footer, Landing, About, Skills, Education, Experience, Contacts, Projects,  Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
     return (
         <div>
             <Helmet>
-                <title>{headerData.name} - Porfolio</title>
+                <title>{headerData.name}</title>
             </Helmet>
-
             <Navbar />        
             <Landing />
             <About />
